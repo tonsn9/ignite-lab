5f1a67ba475039e7eb4e7966600799fb90b8d9f5
Ton Soares <everton.soares@elevenfinancial.com>
2022-06-27 13:40:14 -0300

    Change: implementation video

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: import.meta.env.VITE_API_URL_GRAPHCMS,
+  uri: import.meta.env.VITE_API_URL,
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN_GRAPHCMS}`
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
   },
   cache: new InMemoryCache()
 })
